92b980594f45dc7ceb311b0286973475193b7b62
avvinci <vinciabhinav7@gmail.com>
2019-07-16 13:11:30 +0530

    code reformatted, url list added

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
 const rp = require("request-promise");
-const url = "https://wallpaperplay.com/board/motivational-desktop-wallpapers";
 const cheerio = require("cheerio");
 const download = require("image-downloader");
 
-rp(url)
-  .then(function(html) {
-    const $ = cheerio.load(html);
-    $("a img").attr("data-src")
-    // $("a img").each(function(i, data) {
-    //   // let content = $(data ,'a').attr('href');
-    //   console.log(data);
-    // });
-    let data = $("#82656 a img").html();
-
-    console.log("---", data);
-    console.log("---not done");
-    console.log($("#82656 a img").attr("data-src"));
-    console.log("---done");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-const ImageStart = 82586;
 let options = {
   url: "https://wallpaperplay.com/walls/full/f/8/3/82586.jpg",
   dest: "./images" // Save to /path/to/dest/image.jpg
 };
 
-// async function downloadIMG() {
-//   try {
-//     const { filename, image } = await download.image(options);
-//     console.log(filename); // => /path/to/dest/image.jpg
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
+let NumImages = 10;
+let urlList = [];
+const url = "https://wallpaperplay.com/board/motivational-desktop-wallpapers";
 
-// async function scrapeImages() {
-//   for (let i = 0; i < 10; i++) {
-//     let currentImage = ImageStart + i;
-//     options.url = `https://wallpaperplay.com/walls/full/f/8/3/${currentImage}.jpg`;
-//     await downloadIMG();
-//   }
-// }
+function getUrls() {
+  rp(url)
+    .then(function(html) {
+      const $ = cheerio.load(html);
+      console.log($("a img").attr("data-src"));
 
+      // $("a img").each(function(i, data) {
+      //   // let content = $(data ,'a').attr('href');
+      //   console.log(data);
+      // });
+      const ImageStart = 82586;
+      console.log("---not done");
+      for (let i = 0; i < NumImages; i++) {
+        let id = ImageStart + i;
+        let url = $(`#${id} a img`).attr("data-src");
+        console.log(url);
+        urlList.push(url);
+      }
+      console.log("---done");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+async function downloadIMG() {
+  try {
+    const { filename, image } = await download.image(options);
+    console.log(filename); // => /path/to/dest/image.jpg
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function scrapeImages() {
+  for (let i = 0; i < NumImages; i++) {
+    options.url = `https://wallpaperplay.com/${urlList[i]}`;
+    await downloadIMG();
+  }
+}
+
+getUrls();
 // scrapeImages();
